4e2cd19b2da0d3ea5353f55447a44ca72e897792
Henry Maduekwe <ryhen6931@gmail.com>
2024-10-12 00:14:19 -0700

    Added bg color red to the body

--- branch-folder/workingWithBranch.js
+++ branch-folder/workingWithBranch.js
@@ -3,4 +3,5 @@ button.addEventListener('click', All)
 
 function All(){
     document.getElementById('demo1').textContent = "who the fuck click button1"
+    document.body.style.backgroundColor = 'rgb(225,30,30)'
 }
