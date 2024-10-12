4f029e2324cc6d6fbca8fb43af2a95dc6626dd03
Henry Maduekwe <ryhen6931@gmail.com>
2024-10-12 02:06:28 -0700

    Added scroll container

--- branch-folder/workingWithBranch.js
+++ branch-folder/workingWithBranch.js
@@ -1,6 +1,22 @@
 const button = document.getElementById('btn1')
-button.addEventListener('click', All)
+const scrollCont = document.getElementById('scroll-cont')
+const buttons = document.querySelectorAll('.btn3')
+console.log(buttons)
+buttons.forEach(element => {
+    element.addEventListener('click',()=>{
+        const scrollWidth = scrollCont.clientWidth
+        let scrollAmout = 0
+        if(element.id === 'left'){
+            scrollAmout = -1
+        }
+        else if(element.id === 'right'){
+            scrollAmout = 1;
+        }
+        let combine = scrollWidth*scrollAmout
 
+        scrollCont.scrollBy({left:combine,behavior:"smooth"})
+    })
+});
 function All(){
     document.getElementById('demo1').textContent = "who the fuck click button1"
     document.body.style.backgroundColor = 'rgb(225,30,30)'
