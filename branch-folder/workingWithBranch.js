2c9f27d41fda75503fb1774672599003f75ac771
Henry Maduekwe <ryhen6931@gmail.com>
2024-10-12 00:03:08 -0700

    Added WorkingWithBranch.html  and WorkingWithBranch.css

---
+++ branch-folder/workingWithBranch.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('btn1')
+button.addEventListener('click', All)
+
+function All(){
+    document.getElementById('demo1').textContent = "who the fuck click button1"
+}
